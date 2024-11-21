f643e0dbd9191149db124e5385cf381d39fcb9d5
Adam-WY-Jeong <jwy0805@gmail.com>
2024-11-21 12:31:12 +0900

    To float site on domain

--- APIServer/obj/Debug/net6.0/APIServer.AssemblyInfo.cs
+++ APIServer/obj/Debug/net6.0/APIServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APIServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b9fed1674758f968a1e33995781ce7d8f9d949e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d9ef88acdd4122017525fd97c738aa6239006fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("APIServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APIServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

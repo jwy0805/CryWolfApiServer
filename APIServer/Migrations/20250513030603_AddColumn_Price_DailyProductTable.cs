f0f2426dcb3d19336d59b13f97d02ed6db52fc1f
Adam-WY-Jeong <jwy0805@gmail.com>
2025-05-13 12:12:28 +0900

    1

---
+++ APIServer/Migrations/20250513030603_AddColumn_Price_DailyProductTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddColumn_Price_DailyProductTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Price",
+                table: "DailyProduct",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "DailyProduct");
+        }
+    }
+}

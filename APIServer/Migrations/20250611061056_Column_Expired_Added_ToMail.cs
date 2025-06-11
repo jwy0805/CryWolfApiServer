d836caa74cfa82cc4f14f962f462fc28c0375fe6
Adam-WY-Jeong <jwy0805@gmail.com>
2025-06-11 19:29:00 +0900

    d

---
+++ APIServer/Migrations/20250611061056_Column_Expired_Added_ToMail.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class Column_Expired_Added_ToMail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Expired",
+                table: "Mail",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Expired",
+                table: "Mail");
+        }
+    }
+}

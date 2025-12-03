953a4b1cec758dc6559cb6d585295f08b86e9e0a
Adam-WY-Jeong <jwy0805@gmail.com>
2025-12-03 18:09:43 +0900

    FEAT DB

---
+++ APIServer/Migrations/20251203083230_ADD_COLUMN_USERTAG_TO_USER.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class ADD_COLUMN_USERTAG_TO_USER : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserTag",
+                table: "User",
+                type: "varchar(6)",
+                maxLength: 6,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            // migrationBuilder.CreateIndex(
+            //     name: "IX_User_UserTag",
+            //     table: "User",
+            //     column: "UserTag",
+            //     unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_User_UserTag",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "UserTag",
+                table: "User");
+        }
+    }
+}
---
+++ WebSite/Program.cs
@@ -0,0 +1,45 @@
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.DataProtection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var certPath = Environment.GetEnvironmentVariable("CERT_PATH");
+var certPwd = Environment.GetEnvironmentVariable("CERT_PASSWORD");
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+// -- StartUp.cs - Configure
+if (!builder.Environment.IsDevelopment() && certPath != null && certPwd != null)
+{   
+    // Data Protection
+#pragma warning disable CA1416
+    builder.Services.AddDataProtection()
+        .PersistKeysToFileSystem(new DirectoryInfo("/root/.aspnet/DataProtection-Keys"))
+        .ProtectKeysWithCertificate(new X509Certificate2(certPath, certPwd));
+#pragma warning restore CA1416
+}
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();

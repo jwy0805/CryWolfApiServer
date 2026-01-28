425bff3c97e8124d2ed1a69dedf8d34bd926c7ce
Adam-WY-Jeong <jwy0805@gmail.com>
2026-01-28 18:13:51 +0900

    [FIX] Landing page login error fixed.

--- APIServer/DB/WebPacket.cs
+++ APIServer/DB/WebPacket.cs
@@ -366,7 +366,7 @@ public class LoginUserAccountPacketRequired
 {
     public string UserAccount { get; set; }
     public string Password { get; set; }
-    public string CountryCode { get; set; }
+    public string? CountryCode { get; set; } = "None";
 }
 
 public class LoginUserAccountPacketResponse
@@ -379,7 +379,7 @@ public class LoginUserAccountPacketResponse
 public class LoginApplePacketRequired
 {
     public string IdToken { get; set; }
-    public string CountryCode { get; set; }
+    public string? CountryCode { get; set; } = "None";
 }
 
 public class LoginApplePacketResponse
@@ -392,7 +392,7 @@ public class LoginApplePacketResponse
 public class LoginGooglePacketRequired
 {
     public string IdToken { get; set; }
-    public string CountryCode { get; set; }
+    public string? CountryCode { get; set; } = "None";
 }
 
 public class LoginGooglePacketResponse
@@ -405,7 +405,7 @@ public class LoginGooglePacketResponse
 public class LoginGuestPacketRequired
 {
     public string GuestId { get; set; }
-    public string CountryCode { get; set; }
+    public string? CountryCode { get; set; } = "None";
 }
 
 public class LoginGuestPacketResponse
--- WebSite/obj/Debug/net6.0/WebSite.AssemblyInfo.cs
+++ WebSite/obj/Debug/net6.0/WebSite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebSite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5af2a2cac0577cc1ec7573d0a18e8a28c13e58a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1f52abf254c0c304e7d0f63e0dcef0e8ccfd24a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebSite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebSite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
